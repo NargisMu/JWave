12a688fc211715f9d10a17fbc46d957a152f3d97
nargismu <nargismu@yandex.ru>
2022-06-20 17:24:56 +0300

    edit

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,4 +1,5 @@
 package main;
+
 import org.apache.commons.math3.transform.DftNormalization;
 import org.apache.commons.math3.transform.FastFourierTransformer;
 
@@ -8,4 +9,23 @@ public class Main {
     // приведение с общей длине
 
     // https://www.programcreek.com/java-api-examples/?api=org.apache.commons.math3.transform.FastFourierTransformer
+    public static void loadSignals() {
+        // toDo реализвать чтение сигналов
+        // центрирование сигналов
+        // форматирование файлов -> чтение массивов
+    }
+
+    // сигналы приводятся к общей длине
+    private int padElements(int lengthSignal1, int lengthSignal2) {
+        int max = lengthSignal1;
+        if (lengthSignal1 < lengthSignal2) {
+            max = lengthSignal2;
+        }
+        int base = 1 + (int) Math.floor(log2(max) + 0.4999);
+        return base;
+    }
+
+    public static int log2(int N) {
+        return (int) (Math.log(N) / Math.log(2));
+    }
 }
--- src/main/java/wavelets/Morlet.java
+++ src/main/java/wavelets/Morlet.java
@@ -5,12 +5,12 @@ import interfaces.IWavelet;
 public class Morlet implements IWavelet {
 
     private double Fs;   // частота дискретизации
+    private double dt = 1 / Fs;     // временной шаг
+    private double s0 = 2 * dt;    // минимальный масштаб
     private double J;
     private double w0;          // параметр вейвлета Морлет
     private int voices = 12;
     private int octave = 8;
-    private double dt = 1 / Fs;     // временной шаг
-    private double s0 = 2 * dt;    // минимальный масштаб
 
 
     public Morlet(Double Fs, double w0, int lenthOfSignal) {
@@ -26,12 +26,14 @@ public class Morlet implements IWavelet {
 
     private double[] scales() {
         double a0 = Math.pow(2, 1 / voices);
-        double[] scales = new double[voices * octave - 1];
+        double[] scales = new double[voices * octave];
+        for (int i = 0; i < J * 12; i++) {
+            scales[i] = s0 * a0 * i;
+        }
         return scales;
     }
 
     private double findJ(int lenthOfSignal) {
-
         J = Math.floor(22);
         return J;
     }
